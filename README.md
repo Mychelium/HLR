65548ac94f613655b360826cefeb4faa5a048e0c
m.combes <m.combes@gc-banktm.com>
2020-02-24 11:10:17 +0100

    turning every webpage into a wiki !

--- README.md
+++ README.md
@@ -1,8 +1,28 @@
-## README for MutableFileSystem (MFS)
+# HoloRing 
+
+Holoring is part of the [Planetary OS for Consciousness Evolution][PCE],
+and is the backbone of the new can of blockchain enabling
+[P2P Collective intelligence][P2PCI].
+
+[PCE]: https://duckduckgo.com/?q=Planetary+OS+for=consciousness+evolution
+[P2PCI]: https://github.com/P2PCI-project/P2PCI-Platform/wiki
+[hlring]: https://github.com/P2PCI-project/holoRing
+
+Holorings are comprise of 3 modules:
+
+ - IPFS: immutable content addressable store
+ - IPMS: mutable key values store (permissionned)
+ - CRDT: indepotent merge
+
+# Software folders organization; MutableFileSystem (MFS)
 
 * hlrings (holoring software)
 * ipms (mutable system)
 * etc (miscellaneous)
+
+====
+
+## INSTALLATION
 
 ### Computers w/o bash:
 
@@ -10,6 +30,9 @@
  2. [if necessay (i.e. not included w/ ipfs-desktop above) install [go-ipfs][2] ]
     (<http://127.0.0.1:8080/ipns/dist.ipfs.io/#go-ipfs>)
  3. optionally install a browzer extension [ipfs-companion][3] ([firefox](https://addons.mozilla.org/en-US/firefox/addon/ipfs-companion/) or [chrome](https://chrome.google.com/webstore/detail/ipfs-companion/nibjojkomfdiaoajekhjakgkdhaomnch))
+
+ 4. IPFS Tutorial :
+    create your holoring identity (holoID)
 
 [1]: https://duckduckgo.com=!g+ipfs-desktop
 [2]: https://duckduckgo.com=!g+go-ipfs
---
+++ js/holoresolver.js
@@ -0,0 +1,57 @@
+// glocal wikilink resolver ...
+
+const maps_key = 'QmVuhSv2REdJwMAauvvnTsVbvb95kRXGFprp3gVQUj8z6d';
+const map_url = 'https://ipfs.blockringtm.ml/ipns/' + maps_key + '/wikimap.json';
+
+e = document.getElementById('wiki');
+get_url(e,map_url);
+
+function get_url(e,u) { // e=elements, u=url
+   fetch(u, { mode:'cors' })
+     .then(status)
+     .then( resp => resp.json() )
+     .then( map => {
+        buf = e.innerHTML;
+        let orig = buf;
+        // links : [text info](url)
+        let rex = RegExp('\\[([^\\]]+)\\]\\(([^\\)]+)\\)(?!\')','g')
+        buf = buf.replace(rex,'<a href="'+"$2"+'">'+"$1"+'</a>');
+        for (let key in map) {
+          let href = map[key];
+          // negative lookahead for ' ; [[keyword]]
+          rex = RegExp('\\[\\['+key+'\\]\\](?!\')','g')
+          buf = buf.replace(rex,'<a href="'+href+'">'+key+'</a>');
+          // links : [text info][keyword]
+          rex = RegExp('\\[([^\\]]+)\\]\\['+key+'\\](?!\')','g')
+          buf = buf.replace(rex,'<a href="'+href+'">'+"$1"+'</a>');
+        }
+        // links : [text info][keyref]
+        rex = RegExp('\\[([^\\]]+)\\]\\[([^\\]]+)\\](?!\')','g')
+        buf = buf.replace(rex,"<a target=_blank href=\"https://duckduckgo.com/?q=!g+$2+%2B%23mychelium\">$1</a>");
+        // links : [[keyref]]
+        rex = RegExp('\\[\\[([^\\]]+)\\]\\](?!\')','g')
+        buf = buf.replace(rex,"<a target=_blank href=\"https://duckduckgo.com/?q=!g+$1+%2B%22%23mychelium%22\">$1</a>");
+        // links : #hashtag
+        rex = RegExp('#([a-z][\\S]+)','ig')
+        if (match = buf.match(rex)) { console.log(match) }
+        buf = buf.replace(rex,"<a target=_blank href=\"https://duckduckgo.com/?q=!g+%2B%22%23$1%22\">#$1</a>");
+        e.innerHTML = buf;
+        if (o = document.getElementById('orig')) {
+          o.innerHTML = orig;
+        }  
+      } 
+     )
+   .catch(function(error) {
+      console.log('catch: '+error)
+   });
+
+}
+
+function status(resp) {
+  if (resp.status >= 200 && resp.status < 300) {
+    return Promise.resolve(resp)
+  }
+  return Promise.reject(new Error(resp.statusText))
+}
+ 
+true; 
